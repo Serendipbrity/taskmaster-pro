b770369f0865f44b884d7cc3551cb807ca55c764
Brittani <96275616+Serendipbrity@users.noreply.github.com>
2022-03-29 14:48:07 -0400

    color tasks by due dates

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,6 +13,7 @@ var createTask = function(taskText, taskDate, taskList) {
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
 
+  auditTask(taskLi);
 
   // append to ul list on the page
   $("#list-" + taskList).append(taskLi);
@@ -44,6 +45,26 @@ var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
+var auditTask = function(taskEl) {
+  // get date from task element
+  var date = $(taskEl).find("span").text().trim();
+  console.log(date);
+
+  // convert to moment object at 5:00pm
+  var time = moment(date, "L").set("hour", 17);
+  console.log(time);
+
+  //remove any old classes from element
+  $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
+
+  // apply new class if task is near/over due date
+  if (moment().isAfter(time)) {
+    $(taskEl).addClass("list-group-item-danger");
+  }
+  else if (Math.abs(moment().diff(time,"days")) <= 2 ) {
+    $(taskEl).addClass("list-group-item-warning");
+  }
+};
 
 // modal was triggered
 $("#task-form-modal").on("show.bs.modal", function() {
@@ -145,16 +166,24 @@ $(".list-group").on("click", "span", function() {
     //swap out elements
     $(this).replaceWith(dateInput);
 
+    //enable jquery ui datepicker
+    dateInput.datepicker({
+      minDate: 1,
+      onClose: function(){
+        // when calendar is closed, force a change event on the dateInput
+        $(this).trigger("change");
+      }
+    });
+
     //automatically focus on new element
     dateInput.trigger("focus");
 });
 
 //value of due date was changed
-$(".list-group").on("blur", "input[type='text']", function(){
+$(".list-group").on("change", "input[type='text']", function(){
     // get current text
     var date = $(this)
-       .val()
-       .trim();
+       .val();
 
     // get the parent ul's id attribute
     var status = $(this)
@@ -177,7 +206,10 @@ $(".list-group").on("blur", "input[type='text']", function(){
         .text(date);
 
     // replace input with span element
-    $(this).replaceWith(taskSpan);    
+    $(this).replaceWith(taskSpan);  
+    
+    // pass tasks <li> element into auditTask() to check new due date
+    auditTask($(taskSpan).closest(".list-group-item"));
 });
 // load tasks for the first time
 loadTasks();
@@ -251,4 +283,8 @@ $("#trash").droppable({
   out: function(event, ui) {
     console.log("out");
   }
+});
+
+$("#modalDueDate").datepicker({
+  minDate: 1
 });
